9f4427be5d24b1427abbc104e7ab1085eb02ed5f
Harshabh <Training_b6b.01.09@BDC6-DX-84B6N62.dir.svc.accenture.com>
2017-10-27 09:13:33 +0530

    new commit

---
+++ FestivalPortalR2_Participant/src/com/accenture/adf/test/TestVisitorDAO.java
@@ -0,0 +1,186 @@
+package com.accenture.adf.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.accenture.adf.businesstier.dao.VisitorDAO;
+import com.accenture.adf.businesstier.entity.Visitor;
+import com.accenture.adf.exceptions.FERSGenericException;
+import com.accenture.adf.helper.FERSDataConnection;
+
+/**
+ * JUnit test case for VisitorDAO class for testing all repository methods to
+ * call database sub-routines
+ * 
+ */
+public class TestVisitorDAO {
+
+	private Visitor visitor;
+	private VisitorDAO visitorDAO;
+	private ArrayList<Object[]> registeredEvents;
+
+	/**
+	 * Setting up initial objects
+	 * 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		visitor = new Visitor();
+		visitorDAO = new VisitorDAO();
+		registeredEvents = new ArrayList<Object[]>();
+	}
+
+	/**
+	 * Deallocating objects after execution of every method
+	 * @throws Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		/**
+		 * @TODO: Release all the objects here by assigning them null  
+		 */
+	}
+
+	/**
+	 * Test case for method insertData
+	 */
+	@Test
+	public void testInsertData() {
+		/**
+		 * @TODO: Create visitor object by setting appropriate values
+		 * Call insertData method by passing this visitor object
+		 * Search this new visitor object by calling searchUser method
+		 * Assert the values of username
+		 */		
+	}	
+
+	/**
+	 * Test case for method searchUser
+	 */
+	@Test
+	public void testSearchUser() {
+		/**
+		 * @TODO: Call searchUser method for valid values of username
+		 * and password and assert the value of username for the returned type of method
+		 */		
+	}
+
+	/**
+	 * Test case for method registerVisitorToEvent
+	 */
+	@Test
+	public void testRegisterVisitorToEvent() {
+		/**
+		 * @TODO: Fetch visitor object by calling searchUser for valid values of username and password
+		 * Pass this visitor object and valid eventid to registerVisitorToEvent method
+		 * and assert the value
+		 */		
+		Visitor visitor = new Visitor();
+		VisitorDAO vDao = new VisitorDAO();
+		try {
+			visitor=vDao.searchUser("jjones", "password");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		if(visitor!=null)
+		{
+			
+			try {
+				
+				vDao.registerVisitorToEvent(visitor, 10001,1001);
+				String query = "Select count(*) from eventsessionsignup where visitorid=1003";
+				Connection c = FERSDataConnection.createConnection();
+				PreparedStatement s = c.prepareStatement(query);
+				ResultSet rs=s.executeQuery();
+				rs.next();
+				assertEquals(rs.getInt(1)==1,true);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				//System.out.println(e.getMessage());
+				new FERSGenericException("Unable to Register",new Exception());
+			}
+		}
+		
+		
+	}	
+
+	/**
+	 * Test case for method registeredEvents
+	 */
+	@Test
+	public void testRegisteredEvents() {
+		/**
+		 * @TODO: Fetch visitor object by calling searchUser for valid values of username and password
+		 * Pass this visitor object and valid eventid to registeredEvents method
+		 * and assert the value
+		 */		
+	}
+
+	/**
+	 * Test case for method updateVisitor
+	 */
+	@Test
+	public void testUpdateVisitor() {
+		/**
+		 * @TODO: Fetch visitor object by calling searchUser for valid values of username and password
+		 * Update the value in this visitor object
+		 * Pass this visitor object to updateVisitor method
+		 * and assert the value of changed value
+		 */		
+	}
+
+	/**
+	 * Test case for method registeredEvents
+	 */
+	@Test
+	public void testUnregisterEvent() {
+		/**
+		 * @TODO: Fetch visitor object by calling searchUser for valid values of username and password
+		 * Pass this visitor object and valid eventid to unregisterEvent method
+		 * and assert the value
+		 */		
+	}
+	
+	/**
+	 * Test case for method change password
+	 */
+	/*@Test
+	public void testChangePassword_VisitorNull() {
+		*//**
+		 * @TODO: Call changePassword method by passing visitor object as null
+		 *//*		
+	}*/
+	
+	/**
+	 * Test case for method change password
+	 */
+	@Test
+	public void testChangePassword_VisitorNull() {
+		try {
+			visitor = null;
+			visitorDAO.changePassword(visitor);
+		} catch (SQLException exception) {
+			fail("SQL Exception");
+		} catch (ClassNotFoundException exception) {
+			fail("Class Not Found Exception");
+		} catch (Exception exception) {
+			fail("NULL Exception");
+		}
+	}
+
+}
