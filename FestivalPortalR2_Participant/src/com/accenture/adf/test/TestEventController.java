827300da25541873ac0fb2a9e64a41a1142a699f
Tanish <Training_b6b.01.09@BDC6-DX-83LC152.dir.svc.accenture.com>
2017-10-30 15:12:13 +0530

    TestEventController updated and verified with DB

--- FestivalPortalR2_Participant/src/com/accenture/adf/test/TestEventController.java
+++ FestivalPortalR2_Participant/src/com/accenture/adf/test/TestEventController.java
@@ -8,9 +8,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.accenture.adf.businesstier.controller.EventController;
+import com.accenture.adf.businesstier.dao.VisitorDAO;
 
 /**
  * Junit test class for EventController
@@ -21,18 +23,23 @@ public class TestEventController {
 	private MockHttpServletRequest request;
 	private MockHttpServletResponse response;
 	private ModelAndView modelAndView;
+	
 	private EventController controller;
+
 
 	/**
 	 * Sets up initial objects required in other methods
-	 * 
+	 * 		
 	 * @throws Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
 		modelAndView = new ModelAndView();
 		controller = new EventController();
-		response = new MockHttpServletResponse();		
+		response = new MockHttpServletResponse();	
+
+		
+		
 	}
 
 	/**
@@ -45,6 +52,12 @@ public class TestEventController {
 		/**
 		 * @TODO: Release all the objects here by assigning them null  
 		 */
+		modelAndView =null;
+		controller = null;
+		response = null;
+	
+	
+	
 	}
 
 	/**
@@ -71,6 +84,15 @@ public class TestEventController {
 		 * @TODO: Call getAvailableEvents methods  by passing request as null
 		 * and assert it for appropriate model view name
 		 */	
+		try {
+			request = new MockHttpServletRequest("GET", "/catalog.htm");
+			modelAndView = controller.getAvailableEvents(null, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+		//	fail("Exception");
+			//System.out.println(e.getMessage());
+		}
+		assertEquals(null, modelAndView.getViewName());
 	}
 	
 	/**
@@ -82,6 +104,17 @@ public class TestEventController {
 		 * @TODO: Call displayEvent methods and assert
 		 * it for appropriate model view name
 		 */	
+		try {
+			request = new MockHttpServletRequest("GET", "/displayEvent.htm");
+			request.setParameter("eventId","1001");
+			request.setParameter("sessionId","10001");
+			modelAndView=controller.displayEvent(request,response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//fail("Exception");
+			
+		}
+		assertEquals("/addEvent.jsp",modelAndView.getViewName());
 	}
 
 	/**
@@ -93,6 +126,14 @@ public class TestEventController {
 		 * @TODO: Call displayEvent methods  by passing request as null
 		 * and assert it for appropriate model view name
 		 */	
+		try {
+			request = new MockHttpServletRequest("GET", "/displayEvent.htm");
+			modelAndView = controller.getAvailableEvents(null, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//System.out.println(e.getMessage());
+		}
+		assertEquals(null, modelAndView.getViewName());
 	}	
 	
 	/**
@@ -104,6 +145,28 @@ public class TestEventController {
 		 * @TODO: Call updateEvent methods and assert
 		 * it for appropriate model view name
 		 */	
+		try {
+			request = new MockHttpServletRequest("GET", "/updateEvent.htm");
+			request.setParameter("eventId","1009");
+			request.setParameter("sessionId","10008");
+			request.setParameter("eventName","myevent235");
+			request.setParameter("desc","My Event 525Desc");
+			request.setParameter("place","Bangalore");
+			request.setParameter("duration","3000-5000");
+			request.setParameter("eventType","Casual");
+			request.setParameter("ticket", "255");
+			request.setParameter("isAdd", "true");
+			request.setParameter("coordinator", "105");
+			request.setParameter("eventSession", "2");
+			
+			modelAndView=controller.updateEvent(request, response);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail("Exception");
+		}
+		assertEquals("/addEvent.jsp",modelAndView.getViewName());
 	}
 
 	/**
@@ -115,6 +178,15 @@ public class TestEventController {
 		 * @TODO: Call updateEvent methods  by passing request as null
 		 * and assert it for appropriate model view name
 		 */	
+		try {
+			request = new MockHttpServletRequest("GET", "/updateEvent.htm");
+			modelAndView = controller.getAvailableEvents(null, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//fail("Exception");
+			//e.printStackTrace();
+		}
+		assertEquals(null, modelAndView.getViewName());
 	}
 	
 	/**
@@ -126,7 +198,19 @@ public class TestEventController {
 		 * @TODO: Call deleteEvent methods and assert
 		 * it for appropriate model view name
 		 */
+		try {
+			request = new MockHttpServletRequest("GET", "/deleteEvent.htm");
+			request.setParameter("eventId","1001");
+			request.setParameter("sessionId","10001");
+			modelAndView = controller.deleteEvent(request,response);
+		} catch (Exception exception) {
+			fail("Exception");
+		}
+		assertEquals("/eventCatalog.jsp", modelAndView.getViewName());
 	}
+
+		
+	
 
 	/**
 	 * Executes the negative scenario for displayEvent method
@@ -137,6 +221,15 @@ public class TestEventController {
 		 * @TODO: Call deleteEvent methods  by passing request as null
 		 * and assert it for appropriate model view name
 		 */	
+		try {
+			request = new MockHttpServletRequest("GET", "/deleteEvent.htm");
+			modelAndView = controller.deleteEvent(null, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//fail("Exception");
+		}
+		assertEquals(null, modelAndView.getViewName());
+	}
 	}		
 
-}
+
